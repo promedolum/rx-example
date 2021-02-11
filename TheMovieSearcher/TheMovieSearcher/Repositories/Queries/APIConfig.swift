adf077b59221e5c1789506764581f015045b0dd1
Alexander Zanevskiy <worms.ate.my.brain@gmail.com>
2021-02-11 20:15:32 +0300

    API Config file

---
+++ TheMovieSearcher/TheMovieSearcher/Repositories/Queries/APIConfig.swift
@@ -0,0 +1,9 @@
+//
+//  APIConfig.swift
+//  TheMovieSearcher
+//
+//  Created by Zanevskiy Alexander on 11.02.2021.
+//  Copyright © 2021 Алешин Игорь Эдуардович. All rights reserved.
+//
+
+import Foundation
